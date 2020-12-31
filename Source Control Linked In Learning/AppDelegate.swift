a5d9ea6a442e7b44f4a54e2bb89207859c0857b9
Martin Martin <martin.martin@nyu.edu>
2020-12-30 22:29:31 -0500

    Initial Commit

---
+++ Source Control Linked In Learning/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Source Control Linked In Learning
+//
+//  Created by Martin Martin on 12/30/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Source Control Linked In Learning/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Source Control Linked In Learning
+//
+//  Created by Martin Martin on 12/30/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
